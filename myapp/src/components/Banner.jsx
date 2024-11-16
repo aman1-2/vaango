4f00aad0a85c46f7eba90c3699ddd9c68c34d482
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2024-11-16 09:12:55 +0530

    Updated code.

--- myapp/src/components/Banner.jsx
+++ myapp/src/components/Banner.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import Logo from "../Assets/Vaango_Logo.png";
 import Background from "../Assets/Rectangle_16.png";
+import FoodPhoto from "../Assets/pic-removebg-preview.png";
 
 const Banner = () => {
   return (
-    <div className="relative h-[217px] w-full bg-cover bg-center" style={{ backgroundImage: `url(${Background})` }}>
+    <div
+      className="relative h-[300px] w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
       {/* Banner Content */}
       <div className="relative flex items-center justify-between h-full px-10 z-20">
         
         {/* Left Section - Logo */}
-        <div className="absolute top-[-58px] left-[80px] z-20 flex flex-col items-start space-y-4">
+        <div className="absolute top-[-70px] left-[30px] z-20 items-start space-y-4">
           <img
             src={Logo}
             alt="Logo"
-            className="w-80 h-78 object-contain drop-shadow-mdd"
+            className="w-90 h-80 object-contain drop-shadow-md"
           />
         </div>
 
         {/* Center Section - Food Images */}
-        <div className="flex items-center justify-center">
+        <div className="flex flex-1 items-center justify-center">
           <img
-            src={""}
+            src={FoodPhoto}
             alt="South Indian Dishes"
-            className="w-48 h-auto object-contain rounded-full shadow-lg"
+            className="w-80 h-auto object-contain rounded-full shadow-lg"
           />
         </div>
 
@@ -34,34 +38,6 @@ const Banner = () => {
         </div>
       </div>
     </div>
-
-
-    // // <div className="relative h-[217px] w-full bg-cover bg-center bg-[url('./Assets/Rectangle_16.png')] flex justify-between items-center p-6">
-    // <div className="relative h-[200px] w-full bg-cover bg-center" style={{ backgroundImage: `url(${Background})` }}>
-    //   {/* Left Section */}
-    //   <div >
-    //   {/* Logo Image - Floating above the background */}
-    //   {/* Logo - Positioned to float over both the gradient and navbar */}
-    //   <div className="absolute top-[-50px] left-[100px] z-20 flex flex-col items-start space-y-4">
-    //     <img src={Logo} alt="Logo" className="w-72 h-70 object-contain drop-shadow-md" />
-    //   </div>
-    // </div>
-
-    //   {/* Center Section - Placeholder for South Indian dishes */}
-    //   {/* <div className="flex space-x-2 items-center">
-    //     <div className="w-20 h-20 bg-gray-200 rounded-full flex items-center justify-center">🍛</div>
-    //     <div className="w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center">🍲</div>
-    //     <div className="w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center">🍚</div>
-    //     <div className="w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center">🥞</div>
-    //   </div> */}
-
-    //   {/* Right Section */}
-    //   {/* <div className="flex flex-col justify-center items-start text-right space-y-2 bg-opacity-50 backdrop-blur-sm p-4">
-    //     <p className="text-2xl font-semibold text-yellow-300">Authentic</p>
-    //     <p className="text-lg font-semibold text-yellow-300">Flavors Of</p>
-    //     <p className="text-4xl font-bold text-white">South India</p>
-    //   </div> */}
-    // </div>
   );
 };
 
--- myapp/src/sections/About.jsx
+++ myapp/src/sections/About.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import Truck from '../Assets/truck.png';
+import Dosa from '../Assets/dosa.jpeg';
 
 const About = React.forwardRef((props, ref) => (
   <section ref={ref} className="container mx-auto pl-6 pr-8 ">
-    <div className=' relative flex'>
-      <div>
-        <p className="mt-4 text-lg">
-          Vaango is a one-stop destination for all South Indian food lovers serving pure vegetarian meals in a modern and contemporary ambiance.
-        </p>
-        <p className="mt-4">
-          Vaango essentially means “come in” in Tamil, a quintessential way to welcome guests. Said with a gentle forward bob of the head, a smile, and a Namaskaar, it signifies the opening to a warm and hospitable experience at a South Indian household.
-        </p>
+    <div className=' flex flex-col mt-6 space-y-0'>
+      <div className=' relative flex'>
+        <div>
+          <p className="mt-12 font-semibold text-lg">
+            Vaango is a one-stop destination for all South Indian food lovers serving pure vegetarian meals in a modern and contemporary ambiance.
+          </p>
+          <p className="mt-4">
+            Vaango essentially means “come in” in Tamil, a quintessential way to welcome guests. Said with a gentle forward bob of the head, a smile, and a Namaskaar, it signifies the opening to a warm and hospitable experience at a South Indian household.
+          </p>
+        </div>
+
+        <div>
+          <img src={Truck} alt="Vaango Food Truck" className="z-20 w-100 h-auto max-w-md" />
+        </div>
       </div>
 
-      <div>
-        <img src={Truck} alt="Vaango Food Truck" className="z-20 w-100 h-auto max-w-md" />
+      <div className=' relative flex'>
+        <div>
+          <img src={Dosa} alt="Dosa" className="z-20 w-100 h-auto max-w-md"  />
+        </div>
+
+        <div>
+          <p className="mt-4 text-lg">
+            The first outlet of Vaango opened in NOIDA in 2011 and is focused on the authentic taste and quality South Indian vegetarian food offerings. It has opened over 70 stores in India.
+          </p>
+        </div>
       </div>
     </div>
     
